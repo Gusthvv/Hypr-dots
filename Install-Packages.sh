5ae8707965dd30d979919aa2cc884b8cac6ddc17
Gustavo Merkel B. <gusthvv@gmail.com>
2026-04-26 00:25:36 -0300

    Update Install-Packages.sh

--- Install-Packages.sh
+++ Install-Packages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -e
 
-# Atualiza sistema
+# Atualiza sistema e instala dependências base
 sudo pacman -Syu --noconfirm
-
-# Instala dependências para compilar AUR
 sudo pacman -S --needed git base-devel --noconfirm
 
 # Instala yay
+[ -d yay ] && rm -rf yay
 git clone https://aur.archlinux.org/yay.git
-cd yay || exit
+cd yay
 makepkg -si --noconfirm
 cd ..
 rm -rf yay
@@ -17,11 +17,17 @@ rm -rf yay
 yay -S --needed --noconfirm \
 git curl flatpak unrar unzip 7zip code obs-studio firefox chromium \
 rofi hyprpaper waybar mako fastfetch cava nerd-fonts pavucontrol \
-btop nvim steam pcmanfm jre-openjdk discord wine wine-gecko \
-wine-mono winetricks wl-clipboard cliphist spotify \
-lxappearance grim slurp rocm-smi-lib jq gvfs gvfs-mtp gvfs-smb \
-gvfs-nfs gvfs-afc gvfs-goa gvfs-gphoto2 udisks2 udiskie ntfs-3g \
-dosfstools exfatprogs polkit lxsession tumbler ffmpegthumbnailer \
-jmtpfs libmtp qbittorrent
+btop nvim steam pcmanfm jre-openjdk discord wine winetricks \
+wl-clipboard cliphist spotify lxappearance grim slurp rocm-smi-lib jq \
+gvfs gvfs-mtp gvfs-smb gvfs-nfs gvfs-afc gvfs-goa gvfs-gphoto2 \
+udisks2 udiskie ntfs-3g dosfstools exfatprogs polkit lxsession \
+tumbler ffmpegthumbnailer jmtpfs libmtp qbittorrent
+
+# Flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub org.vinegarhq.Sober
+flatpak install -y flathub com.modrinth.ModrinthApp
+flatpak install -y flathub page.codeberg.libre_menu_editor.LibreMenuEditor
 
 echo "Instalação concluída."
