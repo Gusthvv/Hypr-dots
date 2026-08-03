de91b0cfb0a99b72106b8d2458c1c5fc0e71cda0
Gustavo Merkel B. <gusthvv@gmail.com>
2026-08-03 06:44:39 -0300

    Update Install-Packages.sh

--- Install-Packages.sh
+++ Install-Packages.sh
@@ -15,19 +15,12 @@ rm -rf yay
 
 # Instala todos os pacotes com yay
 yay -S --needed --noconfirm \
-git curl flatpak unrar unzip 7zip code obs-studio firefox chromium \
-rofi hyprpaper waybar mako fastfetch cava nerd-fonts pavucontrol \
-btop nvim steam pcmanfm jre-openjdk discord wine winetricks \
-wl-clipboard cliphist spotify lxappearance grim slurp rocm-smi-lib jq \
-gvfs gvfs-mtp gvfs-smb gvfs-nfs gvfs-afc gvfs-goa gvfs-gphoto2 \
-udisks2 udiskie ntfs-3g dosfstools exfatprogs polkit lxsession \
-tumbler ffmpegthumbnailer jmtpfs libmtp qbittorrent noto-fonts-emoji \
+git code spotify discord steam firefox rofi fastfetch chromium \
+ghostty 
+
 
 # Flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install -y flathub org.vinegarhq.Sober
-flatpak install -y flathub com.modrinth.ModrinthApp
-flatpak install -y flathub page.codeberg.libre_menu_editor.LibreMenuEditor
 
 echo "Instalação concluída."
